4602aa74334fe9d36ff0dbd2742555a2ddb1aea4
Georg Dresler <georg@g-dresler.de>
2025-12-06 16:26:03 +0100

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val operations = input.map {
+            val point = it.substring(1).toInt()
+            if (it.startsWith('L')) {
+                Operation.Left(point)
+            } else {
+                Operation.Right(point)
+            }
+        }
+
+        val dialPositions = operations.fold(listOf(50)) { allPositions, operation ->
+            val newPosition = when (operation) {
+                is Operation.Left -> allPositions.last() - operation.value
+                is Operation.Right -> allPositions.last() + operation.value
+            }.floorMod(100)
+            allPositions + newPosition
+        }
+
+        return dialPositions.count { it == 0 }
     }
 
     fun part2(input: List<String>): Int {
@@ -11,6 +28,17 @@ fun main() {
     part1(testInput).check(3)
 
     val input = readLines("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1(input).println().check(1074)
+//    part2(input).println()
+}
+
+private sealed class Operation {
+    data class Left(val value: Int) : Operation()
+
+    data class Right(val value: Int) : Operation()
+}
+
+private fun Int.floorMod(n: Int): Int {
+    val r = this % n
+    return if (r >= 0) r else r + n
 }
